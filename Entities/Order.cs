804c811bec0f0504c14a96d4f26d2b2fdb110f2a
Gabriel de Brito <gabriel.britog@outlook.com>
2022-06-22 16:54:58 -0300

    Aula 1 - Arquivos organizados e criação de ordem de serviço com Enums

---
+++ Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Aula_Int.Entities.Enums;
+
+namespace Aula_Int.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using Aula_Int.Entities;
+using Aula_Int.Entities.Enums;
 namespace Aula_Int
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
         }
     }
 }
